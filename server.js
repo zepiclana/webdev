b6ddd896203a4e1a5e80f42e4b5c96568fd24908
irmela2412 <irmela.prcic@fet.ba>
2025-06-17 19:15:54 +0200

    Fix product specifications

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const replaceTemplate = (temp, product) => {
   output = output.replace(/{%PRICE%}/g, product.price)
   output = output.replace(/{%FROM%}/g, product.from)
   output = output.replace(/{%ENERGY_SAVINGS%}/g, product.energySavings)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%SPECIFICATIONS%}/g, product.specifications)
   output = output.replace(/{%QUANTITY%}/g, product.quantity)
   output = output.replace(/{%DESCRIPTION%}/g, product.description)
   output = output.replace(/{%ID%}/g, product.id)
